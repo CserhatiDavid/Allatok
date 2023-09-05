28f089fc87ea4e5b8d75ea50a2711b500f802406
CserhatiDavid <cserhatidavid4444@gmail.com>
2023-09-05 12:07:22 +0200

    kert class created and previous app contents moved into Kert.java. App.java is now only makes this thing move

--- src/App.java
+++ src/App.java
@@ -1,14 +1,6 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        Maci maci= new Maci();
-        Cica cica= new Cica();
-        maci.hangotAd("Brummm..");
-        maci.mezgyujtes();
-        //maci.nev="Koda";
-        cica.hangotAd("Nya");
-        cica.dorombol();
-        cica.beallitNev("");
-        //cica.nev="Mirci";
-        //System.out.println(cica.nev);
+        Kert kert=new Kert();
+        kert.inditElet();
     }
 }
---
+++ src/Kert.java
@@ -0,0 +1,14 @@
+public class Kert {
+    public void inditElet() {
+        Maci maci= new Maci();
+        Cica cica= new Cica();
+        maci.hangotAd("Brummm..");
+        maci.mezgyujtes();
+        //maci.nev="Koda";
+        cica.hangotAd("Nya");
+        cica.dorombol();
+        cica.beallitNev("");
+        //cica.nev="Mirci";
+        //System.out.println(cica.nev);
+    }
+}
